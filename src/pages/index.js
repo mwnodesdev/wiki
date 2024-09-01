43e4163d20af609c2d04de0750040d6356823380
mw_dev <79410241+the-mw-dev@users.noreply.github.com>
2024-09-01 18:35:12 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Перейти к базе знаний
           </Link>
         </div>
       </div>
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Расскажем и покажем как что сделать на MWNODES ;)">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
